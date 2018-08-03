c871d30484543e69a2731db3bd01c670ef730189
Jennifer Wilson <jenniferwilson@hxu-macmini-wilsonjl.stanford.edu>
2018-08-02 18:23:33 -0700

    New script to test phen. clust.

---
+++ scripts/run_PathFX_clustering_test.py
@@ -0,0 +1,34 @@
+import pickle,os,csv
+from datetime import datetime
+
+startTime = datetime.now()
+### 1. Enter the name of the analysis
+### 2. Enter the name of the drug. This can be a drug bank ID or the name of drug that isn't in drug bank
+### 3. (Optional) Enter the targets. This only works if you have entered a drug from DrugBank. For investigational drugs, you will need to enter the targets associated with that drug.
+
+analysis_name = 'PathFX_clustering_test'
+
+drug_name = 'metformin'
+cmd = 'python phenotype_enrichment_pathway.py -d %s -a %s -c %s'%(drug_name,analysis_name,'True')
+print(cmd)
+os.system(cmd)
+print(datetime.now() - startTime)
+
+startTime = datetime.now()
+drug_name = 'Montelukast'
+cmd = 'python phenotype_enrichment_pathway.py -d %s -a %s -c %s'%(drug_name,analysis_name,'True')
+print(cmd)
+os.system(cmd)
+print(datetime.now() - startTime)
+
+#drug_name = 'pravastatin'
+#cmd = 'python phenotype_enrichment_pathway.py -d %s -a %s'%(drug_name,analysis_name)
+#os.system(cmd)
+#
+#
+#drug_name = 'propofol'
+#cmd = 'python phenotype_enrichment_pathway.py -d %s -a %s'%(drug_name,analysis_name)
+#os.system(cmd)
+
+
+

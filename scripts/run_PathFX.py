cc240d76bb2263bbd6cd0161bb59fff7554423aa
jenwilson521 <jen.wilson521@gmail.com>
2018-11-05 10:30:55 -0800

    simplified example script

--- scripts/run_PathFX.py
+++ scripts/run_PathFX.py
@@ -2,13 +2,17 @@ import pickle,os,csv
 
 ### 1. Enter the name of the analysis
 ### 2. Enter the name of the drug. This can be a drug bank ID or the name of drug that isn't in drug bank
-### 3. (Optional) Enter the targets. This only works if you have entered a drug from DrugBank. For investigational drugs, you will need to enter the targets associated with that drug.
+### 3. (Optional) Enter the targets. This is only optional if you have entered a drug from DrugBank. For investigational drugs, you will need to enter the targets associated with that drug.
 
 
-analysis_name = 'PathFX_demo'
-drug_name = 'Jenimab'
-drug_targets = 'EGFR,ABL1,ABCB1'
+analysis_name = 'PathFX_analysis'
+drug_name = 'drugimab'
+drug_targets = 'GENE1,GENE2,GENE3'
 
-### call the algorithm
+### call the algorithm without phenotype clustering
 cmd = 'python phenotype_enrichment_pathway.py -d %s -a %s -t %s'%(drug_name,analysis_name,drug_targets)
+os.system(cmd)
+
+### call the algorithm with phenotype clustering
+cmd = 'python phenotype_enrichment_pathway.py -d %s -a %s -c %s'%(drug_name,analysis_name,'True')
 os.system(cmd)

9b1180bff61c85de1084eed7ef663141a91ed538
Jennifer Wilson <jenniferwilson@DNab421184.stanford.edu>
2020-06-16 13:31:08 -0700

    an example code to test install

---
+++ scripts/run_PathFX_test_newLink_0616.py
@@ -0,0 +1,23 @@
+import pickle,os,csv
+
+### 1. Enter the name of the analysis
+### 2. Enter the name of the drug. This can be a drug bank ID or the name of drug that isn't in drug bank
+### 3. (Optional) Enter the targets. This is only optional if you have entered a drug from DrugBank. For investigational drugs, you will need to enter the targets associated with that drug.
+
+
+analysis_name = 'PathFX_test0616'
+drug_name = 'Metformin'
+drug_targets = 'GENE1'
+
+### call the algorithm without phenotype clustering
+cmd = 'python phenotype_enrichment_pathway.py -d %s -a %s -t %s'%(drug_name,analysis_name,drug_targets)
+os.system(cmd)
+
+drug_name = 'FakeDrug'
+drug_targets = 'CDK4,CDK7'
+cmd = 'python phenotype_enrichment_pathway.py -d %s -a %s -t %s'%(drug_name,analysis_name,drug_targets)
+os.system(cmd)
+
+### call the algorithm with phenotype clustering
+#cmd = 'python phenotype_enrichment_pathway.py -d %s -a %s -c %s'%(drug_name,analysis_name,'True')
+#os.system(cmd)
